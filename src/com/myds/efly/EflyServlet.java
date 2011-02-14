31760e5d19ebfeb52ec29f037abdbcfd34637c9d
liaoxw <liaoxw@liaoxw-PC>
2011-02-14 14:27:38 +0800

    is ok

---
+++ src/com/myds/efly/EflyServlet.java
@@ -0,0 +1,12 @@
+package com.myds.efly;
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class EflyServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+		System.out.println(req.getParameter("model"));
+	}
+}
